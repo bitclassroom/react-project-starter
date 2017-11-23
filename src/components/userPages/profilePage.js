c020e8be8494a9edc8a199d2764b4b9561155f1c
BIT Student <classroom@bgit.rs>
2017-11-23 16:55:53 +0100

    Data Service and a bit of Profile Page

--- src/components/userPages/profilePage.js
+++ src/components/userPages/profilePage.js
@@ -1,24 +1,49 @@
 import React, { Component } from "react";
+
 import DataService from "../../services/dataService";
+import RedirectionService from "../../services/redirectionService";
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
 
 class UserProfile extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            name: ""
+            name: "",
+            modalIsOpen: false
         };
 
         this.getData = new DataService();
+        this.redirect = new RedirectionService();
+
+        this.initBind();
+    }
+
+    initBind() {
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     componentDidMount() {
-        this.getData.getProfileData((data) => {
-            console.log(data);
+        this.getData.getProfileData((profile) => {
             this.setState({
-                name: data.data.name
+                name: profile.name
             });
         });
+    }
+
+    openModal() {
+        this.setState({ modalIsOpen: true });
+    }
+
+    afterOpenModal() {
+        this.subtitle.style.color = "#f00";
+    }
+
+    closeModal() {
+        this.setState({ modalIsOpen: false });
     }
 
     render() {
@@ -26,6 +51,25 @@ class UserProfile extends Component {
             <div>
                 <p>{this.state.name}</p>
                 PROFILES
+                <input type="button" id="editProfileData" onClick={this.openModal} value="Edit Profile" />
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    contentLabel="Example Modal"
+                >
+
+                    <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                    <button onClick={this.closeModal}>close</button>
+                    <div>I am a modal</div>
+                    <form>
+                        <input />
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                    </form>
+                </Modal>
             </div>
         );
     }
